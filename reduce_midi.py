5a5702504c7c248e520341aada8979bbc99383ae
ChOoSeMyNaMe <weisar21@googlemail.com>
2020-06-08 18:16:34 +0200

    testing our new algorithm

--- reduce_midi.py
+++ reduce_midi.py
@@ -8,35 +8,36 @@ from common import *
 
 import click
 
-def f_sopran(values: List[int]) -> int:
-    return max(values)
-
-def f_alt(values: List[int]) -> int:
-    upper = max(values)
-    lower = min(values)
-    value = upper - lower
-    value = value * 0.66
-    value = int(value + lower)
-    return value
-
-def f_tenor(values: List[int]) -> int:
-    upper = max(values)
-    lower = min(values)
-    value = upper - lower
-    value = value * 0.33
-    value = int(value + lower)
-    return value
-
-def f_bass(values: List[int]) -> int:
-    return min(values)
-
 RANGE_SOPRAN = [60, 81]
 RANGE_ALT = [52, 74]
 RANGE_TENOR = [46, 69]
 RANGE_BASS = [36, 66]
 
 RANGE_VOICES = [RANGE_SOPRAN, RANGE_ALT, RANGE_TENOR, RANGE_BASS]
-FUNCTION_VOICES = [f_sopran, f_alt, f_tenor, f_bass]
+
+
+def transform_pitches(notes: List[int], double_index: int):
+    return [(note + 12 if i < double_index else note) for i, note in enumerate(notes)]
+
+
+def is_base_pitch(notes: List[int], double_index: int) -> List[int]:
+    values = transform_pitches(notes, double_index)
+    values.sort()
+    for i in range(1, len(values)):
+        if values[0] + 4 == values[i]:
+            for j in range(i + 1, len(values)):
+                if values[i] + 3 == values[j]:
+                    return [values[0], values[i], values[j], values[0] + 12]
+
+
+def get_double_pitch(notes: List[int]) -> int:
+    notes.sort()
+    for i, n in enumerate(notes):
+        for note in notes:
+            if n + 12 == note:
+                return i
+    return -1
+
 
 def get_multiple_number_greater_than(factor: float, target: float):
     tmp = target / factor
@@ -51,6 +52,8 @@ def get_multiple_number_greater_than(factor: float, target: float):
 def get_notes_from_instrument(instr: pretty_midi.Instrument, steps: float) -> Dict[float, List[pretty_midi.Note]]:
     result: Dict[float, List[pretty_midi.Note]] = {}
     for note in instr.notes:
+        if note.duration < steps:
+            continue
         note_time = timespan_from_note(note)
         time = get_multiple_number_greater_than(steps, note_time.start)
         while note_time.contains(time):
@@ -74,34 +77,6 @@ def get_notes_from_midi(mid: pretty_midi.PrettyMIDI, steps: float) -> Dict[float
     return result
 
 
-def voice_contains(voice: List[int], note: int) -> bool:
-    return voice[0] <= note <= voice[1]
-
-
-def list_diff(a: List[any], b: List[any]) -> List[any]:
-    result = []
-    for item in a:
-        if item not in b:
-            result.append(item)
-    return result
-
-
-def get_voices_from_notelist(notes: List[int]) -> List[List[int]]:
-    result = []
-    for voice_index in range(len(RANGE_VOICES)):
-        filtered_voice = []
-        for note in notes:
-            if note is not None and voice_contains(RANGE_VOICES[voice_index], note):
-                filtered_voice.append(note)
-        filtered_voice.sort()
-        result.append(filtered_voice)
-    # filtered_result = []
-    # for i in range(len(RANGE_VOICES)):
-    #     filtered_result.append(list_diff(result[i], result[(i + 1) % len(RANGE_VOICES)]))
-    # return filtered_result
-    return result
-
-
 def split_array(array: List[any], count: int) -> List[List[any]]:
     result = []
     part_length = int(floor(len(array) / count))
@@ -114,35 +89,11 @@ def split_array(array: List[any], count: int) -> List[List[any]]:
     return result
 
 
-def avg(array: List[int]) -> int:
-    return int(floor(sum(array) / len(array)))
-
-
 def normalize_notelists(notes: List[int], desired_note_count: int) -> List[int]:
     if len(notes) == desired_note_count:
         return notes
 
-    voices = get_voices_from_notelist(notes)
-    note_count_per_voice = int(floor(len(RANGE_VOICES) / desired_note_count))
-
-    result = []
-    voice_index = 0
-    for voice in voices:
-        if note_count_per_voice > len(voice):
-            result.extend(voice)
-            missing = note_count_per_voice - len(voice)
-            for _ in range(missing):
-                result.append(None)
-        else:
-            splits = split_array(voice, note_count_per_voice)
-            assert len(splits) == note_count_per_voice
-            for split in splits:
-                # value = avg(split)
-                value = FUNCTION_VOICES[voice_index](split)
-                result.append(value)
-        voice_index += 1
-
-    return result
+    return is_base_pitch(notes, get_double_pitch(notes))
 
 
 def get_note_values_from_midi(mid: pretty_midi.PrettyMIDI, steps: float, desired_note_count: int) -> List[List[int]]:
